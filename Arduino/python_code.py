993086a889b1f48670441ef14bdf784d91311204
ahuja101992 <ahuja101992@gmail.com>
2019-11-13 01:26:48 -0800

    My sql to mongo db

--- Arduino/python_code.py
+++ Arduino/python_code.py
@@ -1,11 +1,15 @@
 import serial 
-import MySQLdb
+#import MySQLdb
 import time
-
+from pymongo import MongoClient
 #establish connection to MySQL. You'll have to change this for your database.
-dbConn = MySQLdb.connect("localhost","root","","rfid_scanner") or die ("could not connect to database")
+#dbConn = MySQLdb.connect("localhost","root","","rfid_scanner") or die ("could not connect to database")
 #open a cursor to the database
-cursor = dbConn.cursor()
+#cursor = dbConn.cursor()
+client = MongoClient("mongodb+srv://root:root@grubhub-b4ptc.mongodb.net/grubhub?retryWrites=true&w=majority")
+#pprint(client.mflix)
+db = client.get_database('Eventify')
+rfid_collection= db.rfid_tags
 
 device = 'COM3' #this will have to be changed to the serial port you are using
 try:
@@ -18,18 +22,22 @@ while True:
     try:
         data=arduino.readline()
         print (data)
-        pieces=data.split(" ")
+        pieces=data.split( )
         print (pieces)
         try:
-            cursor=dbConn.cursor()
-
-            cursor.execute("""INSERT INTO scanner_data (ID,flag) VALUES (%s,%s)""", (pieces[0],pieces[1]))
-            dbConn.commit()
-            cursor.close()
-        except MySQLdb.IntegrityError:
+            new_rfid= {
+            'id':pieces[0].decode('ascii'),
+            'time':'to be fixed',
+            'reader':'1',
+            'flag': pieces[1].decode('ascii')
+            }
+            rfid_collection.insert_one(new_rfid)
+            print("Inserted, new Count", rfid_collection.count_documents({}))
+            clients.close()
+        except :
             print ("failed to insert data")
         finally:
-            cursor.close()
+            clients.close()
     except:
         print ("Processing")
     
