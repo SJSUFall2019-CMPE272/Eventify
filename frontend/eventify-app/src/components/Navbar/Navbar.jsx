7957d57857e699b24bdb5592d4d7edfdcb05f5bb
Chaitanya Naik <chaitanyauday.naik@sjsu.edu>
2019-12-03 03:49:56 -0800

    User Profile on home, options dropdown (logout, edit profile and change password)

--- frontend/eventify-app/src/components/Navbar/Navbar.jsx
+++ frontend/eventify-app/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Dropdown from "react-bootstrap/Dropdown";
 import { withRouter } from "react-router-dom";
 import "./Navbar.scss";
 import { Redirect } from "react-router-dom";
@@ -14,7 +17,10 @@ import {
   faSignOutAlt,
   faSignInAlt,
   faPhoneAlt,
-  faCheck
+  faCheck,
+  faUser,
+  faKey,
+  faUserEdit
 } from "@fortawesome/free-solid-svg-icons";
 
 class Navbar extends Component {
@@ -22,9 +28,23 @@ class Navbar extends Component {
     show: false,
     email: "",
     password: "",
+    firstName: "",
     logoutModal: false,
     fail: false,
-    openSidebar: false
+    openSidebar: false,
+    dropdownOpen: false,
+    profileModal: false,
+    passwordModal: false,
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone_num: "",
+    event_name: "",
+    event_desc: "",
+    currentPass: "",
+    newPass: "",
+    confirmPass: "",
+    passwordError: ""
   };
 
   handleClose = () => {
@@ -54,6 +74,7 @@ class Navbar extends Component {
       if (response.data.authFlag === false) {
         // console.log("testing successful");
         this.setState({ fail: true });
+        return;
       }
       if (response.data.authFlag === true && response.data.type === "Admin") {
         // console.log("testing successful");
@@ -71,15 +92,66 @@ class Navbar extends Component {
         sessionStorage.setItem("email", response.data.email_id);
         this.props.history.push("/userhome");
       }
+      sessionStorage.setItem("firstname", response.data.first_name);
+      this.setState({ firstName: response.data.first_name });
+      this.getProfile();
+    });
+  };
+
+  getProfile() {
+    axios
+      .get("http://localhost:5000/profile/" + sessionStorage.getItem("email"))
+      .then(response => {
+        console.log(response.data.result);
+        let result = response.data.result;
+        this.setState({
+          first_name: result.first_name,
+          last_name: result.last_name,
+          email: result.email,
+          phone_num: result.phone_num,
+          event_name: result.event_name,
+          event_desc: result.event_desc,
+          event_location: result.event_location
+        });
+      });
+  }
+
+  onUpdate = e => {
+    e.preventDefault();
+    let data = {
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      email: this.state.email,
+      phone_num: this.state.phone_num,
+      event_name: this.state.event_name,
+      event_desc: this.state.event_desc
+    };
+    console.log("in update function");
+    axios.post("http://localhost:5000/events/update", data).then(response => {
+      console.log(response.data.message);
+      this.profileClose();
+      window.location.reload(true);
     });
   };
 
   logoutOpen = e => {
-    this.setState({ logoutModal: true });
+    this.setState({ logoutModal: true, dropdownOpen: false });
   };
 
   logoutClose = () => {
     this.setState({ logoutModal: false });
+  };
+
+  logoutOpen = e => {
+    this.setState({ logoutModal: true, dropdownOpen: false });
+  };
+
+  profileClose = () => {
+    this.setState({ profileModal: false });
+  };
+
+  passwordClose = () => {
+    this.setState({ passwordModal: false });
   };
 
   onLogout = e => {
@@ -91,6 +163,27 @@ class Navbar extends Component {
     this.logoutClose();
     this.props.history.push("/");
   };
+
+  resetPassword() {
+    if (this.state.newPass != this.state.confirmPass) {
+      this.setState({ passwordError: true });
+    }
+
+    let data = {
+      email: sessionStorage.getItem("email"),
+      password: this.state.currentPass,
+      new_password: this.state.newPass
+    };
+
+    axios.post("http://localhost:5000/resetPassword", data).then(response => {
+      if (!response.data.message == "Password updated successfully") {
+        this.setState({ passwordError: response.data.message });
+      } else {
+        alert(response.data.message);
+        window.location.reload(true);
+      }
+    });
+  }
 
   render() {
     return (
@@ -120,10 +213,64 @@ class Navbar extends Component {
             (this.props.history.location.pathname != "/" ? "" : "hidden")
           }
         >
-          <div className="login-item flex " onClick={e => this.logoutOpen(e)}>
+          <div
+            className={
+              "login-item flex " + (this.state.dropdownOpen ? "selected" : "")
+            }
+            onClick={e =>
+              this.setState({ dropdownOpen: !this.state.dropdownOpen })
+            }
+          >
+            <FontAwesomeIcon icon={faUser} />
+            {this.state.firstName || sessionStorage.getItem("firstname")}
+          </div>
+          <div
+            className={
+              "dropdown-options " + (this.state.dropdownOpen ? "show" : "")
+            }
+          >
+            <div
+              className={
+                "options flex pointer " +
+                (sessionStorage.getItem("privileges") == "organizer"
+                  ? ""
+                  : "hidden")
+              }
+              onClick={e => {
+                this.getProfile();
+                this.setState({ profileModal: true, dropdownOpen: false });
+              }}
+            >
+              <FontAwesomeIcon icon={faUserEdit} />
+              Edit Profile
+            </div>
+            <div
+              className={
+                "options flex pointer " +
+                (sessionStorage.getItem("privileges") == "organizer"
+                  ? ""
+                  : "hidden")
+              }
+              onClick={e =>
+                this.setState({ passwordModal: true, dropdownOpen: false })
+              }
+            >
+              <FontAwesomeIcon icon={faKey} />
+              Reset Password
+            </div>
+            <div
+              className="options flex pointer"
+              onClick={e => this.logoutOpen(e)}
+            >
+              <FontAwesomeIcon icon={faSignOutAlt} />
+              Logout
+            </div>
+          </div>
+
+          {/* <div className="login-item flex " onClick={e => this.logoutOpen(e)}>
             <FontAwesomeIcon icon={faSignOutAlt} />
             Logout
-          </div>
+          </div> */}
         </div>
 
         <div
@@ -180,6 +327,8 @@ class Navbar extends Component {
           </div>
         </div>
 
+        {/* LOGIN MODAL */}
+
         <Modal show={this.state.show} onHide={this.handleClose} centered>
           <Modal.Header closeButton>
             <Modal.Title>Login</Modal.Title>
@@ -225,6 +374,8 @@ class Navbar extends Component {
             </Button>
           </Modal.Footer>
         </Modal>
+
+        {/* LOGOUT MODAL */}
         <Modal centered show={this.state.logoutModal} onHide={this.logoutClose}>
           <Modal.Header closeButton>
             <Modal.Title id="contained-modal-title-vcenter">Logout</Modal.Title>
@@ -241,6 +392,220 @@ class Navbar extends Component {
               <FontAwesomeIcon icon={faTimes} />
               No
             </Button>
+          </Modal.Footer>
+        </Modal>
+
+        {/* EDIT PROFILE MODAL */}
+        <Modal
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          show={this.state.profileModal}
+          onHide={this.profileClose}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Edit Profile
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="tab-body">
+              <Form
+                noValidate
+                validated={this.state.validated}
+                // onSubmit={e => this.handleSubmit(e)}
+              >
+                <Form.Group as={Row}>
+                  <Form.Label column md={4}>
+                    First name
+                  </Form.Label>
+
+                  <Col sm={8}>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder="First name"
+                      value={this.state.first_name}
+                      onChange={e =>
+                        this.setState({ first_name: e.target.value })
+                      }
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please enter first name.
+                    </Form.Control.Feedback>
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row}>
+                  <Form.Label column md={4}>
+                    Last name
+                  </Form.Label>
+
+                  <Col sm={8}>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder="Last name"
+                      value={this.state.last_name}
+                      onChange={e =>
+                        this.setState({ last_name: e.target.value })
+                      }
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please enter last name.
+                    </Form.Control.Feedback>
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row}>
+                  <Form.Label column md={4}>
+                    Phone Number
+                  </Form.Label>
+
+                  <Col sm={8}>
+                    <Form.Control
+                      required
+                      type="number"
+                      placeholder="Phone Number"
+                      value={this.state.phone_num}
+                      onChange={e =>
+                        this.setState({ phone_num: e.target.value })
+                      }
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please enter phone number
+                    </Form.Control.Feedback>
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row}>
+                  <Form.Label column md={4}>
+                    Event Name
+                  </Form.Label>
+
+                  <Col sm={8}>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder="Event Name"
+                      value={this.state.event_name}
+                      onChange={e =>
+                        this.setState({ event_name: e.target.value })
+                      }
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please enter Event Name
+                    </Form.Control.Feedback>
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row}>
+                  <Form.Label column md={4}>
+                    Event Description
+                  </Form.Label>
+
+                  <Col sm={8}>
+                    <Form.Control
+                      required
+                      as="textarea"
+                      placeholder="Event Description"
+                      rows="3"
+                      value={this.state.event_desc}
+                      onChange={e =>
+                        this.setState({ event_desc: e.target.value })
+                      }
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please enter Event Description
+                    </Form.Control.Feedback>
+                  </Col>
+                </Form.Group>
+              </Form>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              onClick={e => {
+                this.onUpdate(e);
+              }}
+            >
+              Update
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+        <Modal
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          show={this.state.passwordModal}
+          onHide={this.passwordClose}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Reset Password
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div
+              className={
+                "text-danger " +
+                (this.state.passwordError != "" ? "" : "hidden")
+              }
+            >
+              <p>
+                <center>{this.state.passwordError}</center>
+              </p>
+            </div>
+            <Form.Group as={Row}>
+              <Form.Label column md={4}>
+                Current Password
+              </Form.Label>
+
+              <Col sm={8}>
+                <Form.Control
+                  required
+                  type="password"
+                  placeholder="Password"
+                  value={this.state.currentPass}
+                  onChange={e => this.setState({ currentPass: e.target.value })}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row}>
+              <Form.Label column md={4}>
+                New Password
+              </Form.Label>
+
+              <Col sm={8}>
+                <Form.Control
+                  required
+                  type="password"
+                  placeholder="Password"
+                  value={this.state.newPass}
+                  onChange={e => this.setState({ newPass: e.target.value })}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row}>
+              <Form.Label column md={4}>
+                Confirm Password
+              </Form.Label>
+
+              <Col sm={8}>
+                <Form.Control
+                  required
+                  type="password"
+                  placeholder="Password"
+                  value={this.state.confirmPass}
+                  onChange={e => this.setState({ confirmPass: e.target.value })}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter Password.
+                </Form.Control.Feedback>
+              </Col>
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={e => this.resetPassword(e)}>Submit</Button>
           </Modal.Footer>
         </Modal>
       </div>
--- frontend/eventify-app/src/components/Users/UserHome.jsx
+++ frontend/eventify-app/src/components/Users/UserHome.jsx
@@ -1,24 +1,143 @@
 import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 class UserHome extends Component {
   state = {};
   render() {
     console.log(this.props.profile);
     return (
-      <div className="tab-content">
-        <h1 className="header">User Home</h1> <div class="border-div"></div>
+      <div className="tab-content user-profile">
+        <h1 className="header">Welcome to Eventify! </h1>
+        <div class="border-div"></div>
         <p>
-        {this.props.profile.first_name}<br/>
-        {this.props.profile.last_name}<br/>
-        {this.props.profile.email}<br/>
-        {this.props.profile.phone_num}<br/>
-        {this.props.profile.event_name}<br/>
-        {this.props.profile.event_desc}<br/>
-        {this.props.profile.event_location}<br/>
-        {new Date(this.props.profile.event_date_from).toLocaleDateString()}<br/>
-        {new Date(this.props.profile.event_date_to).toLocaleDateString()}
-
+          Please verify your details as shown below and use the options menu to
+          change profile details or reset your password.
         </p>
+        <p></p>
+        <Form className="col-sm-6 profile-form">
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="6">
+              First Name:
+            </Form.Label>
+            <Col sm="6">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue={this.props.profile.first_name}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="6">
+              Last Name:
+            </Form.Label>
+            <Col sm="6">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue={this.props.profile.last_name}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="6">
+              Email:
+            </Form.Label>
+            <Col sm="6">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue={this.props.profile.email}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="6">
+              Phone Number:
+            </Form.Label>
+            <Col sm="6">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue={this.props.profile.phone_num}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="6">
+              Event Name:
+            </Form.Label>
+            <Col sm="6">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue={this.props.profile.event_name}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="6">
+              Event Description:
+            </Form.Label>
+            <Col sm="6">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue={this.props.profile.event_desc}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="6">
+              Event Location:
+            </Form.Label>
+            <Col sm="6">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue={this.props.profile.event_location}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="6">
+              Event From:
+            </Form.Label>
+            <Col sm="6">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue={new Date(
+                  this.props.profile.event_date_from
+                ).toLocaleDateString()}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="6">
+              Event To:
+            </Form.Label>
+            <Col sm="6">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue={new Date(
+                  this.props.profile.event_date_to
+                ).toLocaleDateString()}
+              />
+            </Col>
+          </Form.Group>
+        </Form>
       </div>
     );
   }
